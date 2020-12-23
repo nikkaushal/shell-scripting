8a36b53ab0cdbd2c3beb48d88d254d2a7456df41
nikkaushal <nikkaushal@gmail>
2020-12-22 20:45:07 -0500

    test 2

--- 02-print.sh
+++ 02-print.sh
@@ -4,4 +4,24 @@
 
 echo hellow world
 echo Good morningss
-echo -e "hello, \n\n welcome to todays session, \n\n regards,\n nikk"
+
+#escape sequencing
+#\n new line
+#\t tab
+#\e new color
+echo -e "hello, \n\n\twelcome to todays session, \n\n regards,\n nikk"
+
+#colors- Background
+# red 31, 41
+#green 32, 42
+# yellow 33, 43
+# blue 34, 44
+# msgenta 35, 45
+# cyan 36,46
+
+#echo -e "\e[COLmMessage"
+
+echo -e "\e[31mRED Text"
+echo -e "\e[32mGREEN Text"
+echo -e "\e[33YELLOW Text"
+echo -e "\e[41; 33mYELLOW Text on RED"
