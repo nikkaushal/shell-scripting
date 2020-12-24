24d5d864ecf8cfbaa25ada4c69dd6ec1e89cd3e8
nikkaushal <nikkaushal@gmail>
2020-12-24 10:59:31 -0500

    speacial vars

--- 05-special-vars.sh
+++ 05-special-vars.sh
@@ -5,7 +5,12 @@
 #This is the approach taken by most of the commands in the shell
 #special variables are $0-$n, $*,$@. $#
 #$0 = script name
-#$1-n = arguments passed
 echo script name = $0
+
+#$1-n = arguments passed
+
 echo first arg =$1
 echo second arg= $2
+
+echo "all arguments (*)= $*"
+echo "all argumemts (@) = $@"
