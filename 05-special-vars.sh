be4f7bd17fe9ea4e77275df86169764d80b9e952
nikkaushal <nikkaushal@gmail>
2020-12-24 10:54:33 -0500

    speacial vars

---
+++ 05-special-vars.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+#Inputs before execution and those inputs can be loaded by script
+#Those values will be loaded by special variables inside shell
+
+#This is the approach taken by most of the commands in the shell
+#special variables are $0-$n, $*,$@. $#
+
+echo script name = $0
