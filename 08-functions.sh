c486f9953aff1f52e627a5b76efcd09a151d19d9
nikkaushal <nikkaushal@gmail>
2020-12-24 19:30:16 -0500

    FUNCTIONS

---
+++ 08-functions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#assign a name to date is variable
+#assing a name to a group pf commands is function
+#way1
+function SAMPLE() {
+  echo Hello from sample function
+}
+
+#way2
+
+SAMPLE1() {
+  echo hello from function sample1
+}
+#gunction name standards are as same as variable in bash shell
+
+#call a function
+
+SAMPLES
+SAMPLE1
