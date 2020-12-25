54abf11576c53572e6d36191523d78e26756ae65
nikkaushal <nikkaushal@gmail>
2020-12-24 19:24:18 -0500

    roboshop project components

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,3 +1,6 @@
 #!bin/bash
 
-echo -e "[\e[1;34minfo\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] installing frontend component"
+echo -e "[\e[1;34mINFO\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] setup frontend component"
+echo -e "[\e[1;34mINFO\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] installing nginx"
+echo -e "[\e[1;32mSUCCESS\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] installed nginx"
+echo -e "[\e[1;31mFAILURE\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] installed nginx"
