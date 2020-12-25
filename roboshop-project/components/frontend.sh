c6f66caae5c0cc670f33a183e94ec2795adf33b9
nikkaushal <nikkaushal@gmail>
2020-12-24 19:55:12 -0500

    roboshop

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -11,12 +11,11 @@ FAIL (){
 echo -e "[\e[1;31mFAILURE\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] $1"
 }
 
-echo -e "[\e[1;34mINFO\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] installing nginx"
 
 
 
 
 INFO "setup frontend component"
-INFO "installin g nginx"
+INFO "installing nginx"
 SUCCESS "installed nginx"
 FAIL "installed nginx"
