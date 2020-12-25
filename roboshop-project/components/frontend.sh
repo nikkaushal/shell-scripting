e5ab25e10afbe164c1f4bf23c4d70c9d4dee0d8d
nikkaushal <nikkaushal@gmail>
2020-12-24 19:20:33 -0500

    roboshop project components

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 
-echo -e "[\e[1;34minfo\e[0m] [\e1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] installing frontend component"
+echo -e "[\e[1;34minfo\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] installing frontend component"
