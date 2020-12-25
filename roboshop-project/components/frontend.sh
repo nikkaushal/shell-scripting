b58fc50d76def1d9341ac068cac07e54221fcc80
nikkaushal <nikkaushal@gmail>
2020-12-24 19:52:23 -0500

    roboshop

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,6 +1,22 @@
 #!bin/bash
+INFO () {
+echo -e "[\e[1;34mINFO\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] $1"
+}
 
-echo -e "[\e[1;34mINFO\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] setup frontend component"
+SUCCESS() {
+  echo -e "[\e[1;32mSUCCESS\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] $1"
+}
+
+FAIL (){
+echo -e "[\e[1;31mFAILURE\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] $1"
+}
+
 echo -e "[\e[1;34mINFO\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] installing nginx"
-echo -e "[\e[1;32mSUCCESS\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] installed nginx"
-echo -e "[\e[1;31mFAILURE\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] installed nginx"
+
+
+
+
+INFO "setup frontend component"
+INFP "installing nginx"
+SUCESS "installed nginx"
+FAIL "installed nginx"
