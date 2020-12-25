fdb60a025ba800867a3354976b27d1e77018068d
nikkaushal <nikkaushal@gmail>
2020-12-24 19:35:22 -0500

    FUNCTIONS

--- 08-functions.sh
+++ 08-functions.sh
@@ -5,16 +5,23 @@
 #way1
 function SAMPLE() {
   echo Hello from sample function
-}
+  a=10
+  echo B=$b
 
 #way2
 
 SAMPLE1() {
   echo hello from function sample1
 }
-#gunction name standards are as same as variable in bash shell
+#function name standards are as same as variable in bash shell
 
-#call a function
-
+#call a function in main program
+b=20
 SAMPLE
 SAMPLE1
+echo A =$a
+
+#declare variable in main program, you can access in function and vice-versa
+
+#you delcare a variable in main program and you can overwrite variable in function and vice-versa
+
