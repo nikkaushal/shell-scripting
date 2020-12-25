b436b38feb4a1d63dd5f6a0b6bd32a3569aeb215
nikkaushal <nikkaushal@gmail>
2020-12-24 19:58:25 -0500

    roboshop

---
+++ roboshop-project/components/common.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+INFO () {
+echo -e "[\e[1;34mINFO\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] $1"
+}
+
+SUCCESS() {
+  echo -e "[\e[1;32mSUCCESS\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] $1"
+}
+
+FAIL (){
+echo -e "[\e[1;31mFAILURE\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] $1"
+}
+
--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,19 +1,5 @@
 #!bin/bash
-INFO () {
-echo -e "[\e[1;34mINFO\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] $1"
-}
-
-SUCCESS() {
-  echo -e "[\e[1;32mSUCCESS\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] $1"
-}
-
-FAIL (){
-echo -e "[\e[1;31mFAILURE\e[0m] [\e[1;35mfrontend\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] $1"
-}
-
-
-
-
+source common.bash
 
 INFO "setup frontend component"
 INFO "installing nginx"
