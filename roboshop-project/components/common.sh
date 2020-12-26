ad24b4321b9b76d489dd31faa804ad28dd0ebd9c
nikkaushal <nikkaushal@gmail>
2020-12-26 13:55:06 -0500

    roboshop

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -11,3 +11,14 @@ FAIL (){
 echo -e "[\e[1;31mFAILURE\e[0m] [\e[1;35m${COMPONENT}\e[0m]  [\e[1;36m$(date '+%F %T')\e[0m] $1"
 }
 
+#verify user is root or not
+USER_ID =$(id-u)
+case $USER_ID IN
+  0)
+  true ##nothing to perform so we choose true
+  ;;
+ *)
+   echo -e "\e[1;31m you should be root user to perform this script\e[0m"
+   exit 1
+   ;;
+ esac
