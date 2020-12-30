1c20ddf153a255591ee7742d19729384a51d4ca0
nikkaushal <nikkaushal@gmail>
2020-12-29 21:01:12 -0500

    Roboshop added again

--- roboshop-project/components/payment.sh
+++ roboshop-project/components/payment.sh
@@ -3,3 +3,5 @@
 COMPONENT=payment
 
 source components/common.sh
+
+
--- roboshop-project/components/rabbitmq.sh
+++ roboshop-project/components/rabbitmq.sh
@@ -3,3 +3,26 @@
 COMPONENT=rabbitmq
 
 source components/common.sh
+
+INFO "Erlang is a dependency which is needed for RabbitMQ."
+yum install https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm -y &>>$LOG_FILE
+STAT $? "Erlang is a dependency which is needed for RabbitMQ"
+
+INFO "Setup YUM repositories for RabbitMQ"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG_FILE
+STAT $? "Setup YUM repositories for RabbitMQ"
+
+INFO "Install RabbitMQ server"
+yum install rabbitmq-server -y &>>$LOG_FILE
+STAT $? "Install RabbitMQ server"
+
+INFO "Start RabbitMQ service"
+systemctl enable rabbitmq-server &>>$LOG_FILE
+systemctl start rabbitmq-server &>>$LOG_FILE
+STAT $? "Start RabbitMQ service"
+
+INFO "Create roboshop app user in rabbitmq"
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_user_tags roboshop administrator
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+STAT $? "roboshop app user create"
