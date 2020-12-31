d0a468f6e65dc282a4e45e46640db1ff02621402
nikkaushal <nikkaushal@gmail>
2020-12-30 20:14:46 -0500

    Roboshop added again

--- roboshop-project/launch-instances.sh
+++ roboshop-project/launch-instances.sh
@@ -1,3 +1,11 @@
 #!bin/bash
-
-aws ec2 run-instances \ --launch-template LaunchTemplateId=lt-0a24e440439a9d8a1 \ --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]"
+case $1 in
+  launch)
+    for component in frontend cart catalogue user rabbitmq reddis payment mysql mongo redis; do
+    aws ec2 run-instances \ --launch-template LaunchTemplateId=lt-0a24e440439a9d8a1 \ --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]"
+  ;;
+  routes)
+    echo updating routes
+  ;;
+esac
+done
