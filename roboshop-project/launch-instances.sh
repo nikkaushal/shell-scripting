1ee3fdce9c2be6300f0b934ade4418366be4216d
nikkaushal <nikkaushal@gmail>
2021-01-01 15:49:20 -0500

    Roboshop-project commit

--- roboshop-project/launch-instances.sh
+++ roboshop-project/launch-instances.sh
@@ -2,14 +2,14 @@
 
 case $1 in
   launch)
-    for component in frontend cart catalogue user rabbitmq reddis payment mysql mongo redis; do
+    for component in frontend cart catalogue user rabbitmq shipping payment mysql mongo redis; do
     echo "launching $component spot instances"
     aws ec2 run-instances --launch-template LaunchTemplateId=lt-0a24e440439a9d8a1 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
     done
    ;;
   routes)
     echo updating routes
-    for component in frontend cart catalogue user rabbitmq reddis payment mysql mongo redis; do
+    for component in frontend cart catalogue user rabbitmq shipping payment mysql mongo redis; do
     IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq '.Reservations[].Instances[].PrivateIpAddress')
    echo $component $IP
    sed -e "s/IPADDRESS/${IP}/" -e "s/COMPONENT/${component}/" record.json >/tmp/${component}.json
